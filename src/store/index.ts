12f124f205c48a0c361075a0ca1f6be88894ad8a
erissa1997 <saitie2007@gmail.com>
2025-03-23 18:22:00 +0900

    init store/ type

---
+++ src/store/index.ts
@@ -0,0 +1,2 @@
+// import { defineStore } from "pinia";
+// import { TimelineItem, BlogPost, Service, ExperienceArea } from "@/types";
---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+// export interface TimelineItem {
+//   id: number;
+//   title: string;
+//   subtitle: string;
+//   description: string;
+//   date: string;
+//   type: "education" | "professional" | "other";
+// }
+
+// export interface BlogPost {
+//   id: number;
+//   title: string;
+//   summary: string;
+//   content: string;
+//   date: string;
+//   tags: string[];
+//   imageUrl?: string;
+// }
+
+// export interface Service {
+//   id: number;
+//   title: string;
+//   description: string;
+//   icon: string;
+// }
+
+// export interface ExperienceArea {
+//   id: number;
+//   title: string;
+//   description: string;
+//   icon: string;
+// }
